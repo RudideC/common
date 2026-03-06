547247d730b850c0c9d0e5b6aa24b9fa15892153
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-06 12:45:22 +0100

    Transitioned mod helpers to common

---
+++ src/main/java/org/saturnclient/modules/ModManager.java
@@ -0,0 +1,52 @@
+package org.saturnclient.modules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.saturnclient.modules.mods.*;
+
+public class ModManager {
+    public static final List<Module> ENABLED_MODS = new ArrayList<>();
+    public static final Module[] ALL_MODS = {
+            new Crosshair(),
+            new AutoSprint(),
+            new ArmorDisplay(),
+            new Fps(),
+            new StatusEffects(),
+            new Coordinates(),
+            new Freelook(),
+            new Keystrokes(),
+            new Fullbright(),
+            new NoFog(),
+            new DayCounter(),
+            new Speedometer(),
+            new Zoom(),
+            new HealthDisplay(),
+            new Nametags(),
+            new Ping(),
+            new Tps(),
+            new Clock()
+    };
+
+    public static void init() {
+        // Pre-filter enabled modules to avoid checking every tick
+        updateEnabledModules();
+    }
+
+    public static synchronized void updateEnabledModules() {
+        ENABLED_MODS.clear();
+        for (Module m : ALL_MODS) {
+            if (m.isEnabled()) {
+                ENABLED_MODS.add(m);
+            }
+        }
+    }
+
+    public static Module[] getAllMods() {
+        return ALL_MODS;
+    }
+
+    public static void refreshEnabledModules() {
+        updateEnabledModules();
+    }
+}
