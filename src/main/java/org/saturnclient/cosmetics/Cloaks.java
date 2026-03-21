9c2d5051c5caf87b1ad6243811fd1a598240b5e0
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-21 01:54:17 +0100

    Albania mark

--- src/main/java/org/saturnclient/cosmetics/Cloaks.java
+++ src/main/java/org/saturnclient/cosmetics/Cloaks.java
@@ -23,7 +23,7 @@ import org.saturnclient.cosmetics.utils.GifDecoder;
  */
 public class Cloaks {
     public static final String[] ALL_CLOAKS = { "glitch", "mercedes_flow", "crimson_mark", "bmw", "amg", "amg_petronas",
-            "ferrari", "redbull", "black_hole_amethyst", "black_hole_flame", "black_hole_white" };
+            "ferrari", "redbull", "black_hole_amethyst", "black_hole_flame", "black_hole_white", "albania_mark" };
     private static final String[] ANIMATED_CLOAKS = { "glitch", "black_hole_amethyst", "black_hole_flame",
             "black_hole_white" };
 
